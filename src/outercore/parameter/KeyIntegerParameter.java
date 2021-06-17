66dbb9268efad6126ce4f309634869e51ec60827
Walter <walterxie@users.noreply.github.com>
2021-06-18 11:38:39 +1200

    change java 1.8 code

--- src/outercore/parameter/KeyIntegerParameter.java
+++ src/outercore/parameter/KeyIntegerParameter.java
@@ -3,6 +3,8 @@ package outercore.parameter;
 import beast.core.Input;
 import beast.core.parameter.IntegerParameter;
 
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 /**
@@ -13,7 +15,7 @@ import java.util.List;
 public class KeyIntegerParameter extends IntegerParameter implements KeyParameter<Integer> {
 
     public final Input<String> dimNamesInput = new Input<>("dimNames",
-            "the unique dimension names for the dimensions of this parameter, " +
+            "(Deprecated) the unique dimension names for the dimensions of this parameter, " +
                     "which can also be column names when values are 2d matrix. ",
             null, Input.Validate.XOR, keysInput);
 
@@ -28,8 +30,9 @@ public class KeyIntegerParameter extends IntegerParameter implements KeyParamete
 
         if (dimNamesInput.get() != null) {
             String[] dimNamesArr = dimNamesInput.get().split(" ");
-            // Unmodifiable List
-            this.dimNames = List.of(dimNamesArr);
+            // unmodifiable list : UnsupportedOperationException if attempting to modify
+//            this.dimNames = List.of(dimNamesArr);// > java 1.8
+            this.dimNames = Collections.unmodifiableList(Arrays.asList(dimNamesArr));
         }
 
         dimNameToIndexMap = initDimNames(dimNames,this);
@@ -48,9 +51,19 @@ public class KeyIntegerParameter extends IntegerParameter implements KeyParamete
      * @return the array of keys (a unique string for each dimension) that parallels the parameter index.
      */
     public String[] getKeys() {
-        if (dimNames != null) return dimNames.toArray(String[]::new);
+        if (dimNames != null) return dimNames.toArray(new String[0]);
         return super.getKeys();
     }
+
+    /**
+     * @return the unmodifiable list of keys (a unique string for each dimension)
+     *         that parallels the parameter index.
+     *         It will throw UnsupportedOperationException if attempting to modify
+     */
+//    public List<String> getKeysList() {
+//        if (dimNames != null) return dimNames; // unmodifiable list
+//        return super.getKeysList();
+//    }
 
     /**
      * @param key unique key for a value
